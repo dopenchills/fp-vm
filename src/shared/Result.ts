c04af9fcc99abfbed62d7fdf1c18be8b193bb47f
besaklish <besaklish@gmail.com>
2025-07-03 07:28:12 +0900

    add map

--- src/shared/Result.ts
+++ src/shared/Result.ts
@@ -2,21 +2,27 @@ export type Result<T, E = Error> =
 | {
     ok: true,
     value: T
+    map<U>(fn: (value: T) => U): Result<U, E>
   }
 | {
     ok: false
     error: E
+    map<U>(fn: (value: T) => U): Result<U, E>
   }
 
-export const ok = <T, E = Error>(value: T): Result<T, E> => ({ ok: true, value })
-export const err = <T, E = Error>(error: E): Result<T, E> => ({ ok: false, error })
-
-export const map = <T, U, E>(
-  fn: (value: T) => U,
-  result: Result<T, E>
-): Result<U, E> => {
-  if (result.ok) {
-    return ok<U, E>(fn(result.value))
+export const ok = <T, E = Error>(value: T): Result<T, E> => ({ 
+  ok: true, 
+  value,
+  map<U>(fn: (value: T) => U): Result<U, E> {
+    return ok<U, E>(fn(value))
   }
-  return result
-}
+})
+
+export const err = <T, E = Error>(error: E): Result<T, E> => ({ 
+  ok: false, 
+  error,
+  map<U>(_fn: (value: T) => U): Result<U, E> {
+    return err<U, E>(error)
+  }
+})
+
