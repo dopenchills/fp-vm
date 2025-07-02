ba39e34862bf56dfd6de3028369f70be64c8647b
besaklish <besaklish@gmail.com>
2025-07-03 07:26:38 +0900

    fix result

--- src/shared/Result.ts
+++ src/shared/Result.ts
@@ -8,5 +8,15 @@ export type Result<T, E = Error> =
     error: E
   }
 
-export const ok = <T>(value: T): Result<T> => ({ ok: true, value })
+export const ok = <T, E = Error>(value: T): Result<T, E> => ({ ok: true, value })
 export const err = <T, E = Error>(error: E): Result<T, E> => ({ ok: false, error })
+
+export const map = <T, U, E>(
+  fn: (value: T) => U,
+  result: Result<T, E>
+): Result<U, E> => {
+  if (result.ok) {
+    return ok<U, E>(fn(result.value))
+  }
+  return result
+}
