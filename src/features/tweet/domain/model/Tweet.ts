99e42f85c98f9c952a1d6b5fb6f1cd1c947a6b31
besaklish <besaklish@gmail.com>
2025-07-05 13:50:29 +0900

    add Tweet

---
+++ src/features/tweet/domain/model/Tweet.ts
@@ -0,0 +1,19 @@
+import { err, ok, type Result } from "src/shared/Result"
+
+export type Tweet = {
+  content: string
+}
+
+const maxLength = 140
+
+export const tweet = (value: { content: string }): Result<Tweet, string> => {
+  if (value.content.length === 0) {
+    return err('Tweet something')
+  }
+
+  if (value.content.length > maxLength) {
+    return err('Tweet until 140 chars')
+  }
+
+  return ok(value)
+}
