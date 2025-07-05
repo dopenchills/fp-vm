d7ac2524cca8c630423c84688c101c31751e1da4
besaklish <besaklish@gmail.com>
2025-07-05 14:50:24 +0900

    update useIsBusy to return type

--- src/shared/views/composables/useIsBusy.ts
+++ src/shared/views/composables/useIsBusy.ts
@@ -3,13 +3,17 @@ import { ref } from 'vue'
 export const useBusy = () => {
   const isBusy = ref(false)
   
-  const withBusy = async (process: () => Promise<any>): Promise<void> => {
+  const withBusy = async <T>(process: () => Promise<T>): Promise<T> => {
     isBusy.value = true
+
+    let result: T
     try {
-      await process()
+      result = await process()
     } finally {
       isBusy.value = false
     }
+
+    return result
   }
   
   return { isBusy, withBusy }
