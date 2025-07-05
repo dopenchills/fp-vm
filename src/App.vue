7922d8f4a26a5c6301747046c7fab0f6ca4ebbdf
besaklish <besaklish@gmail.com>
2025-07-05 15:21:54 +0900

    add TweetComponent with bug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <SimpleCounter />
+  <div>
+    <SimpleCounter />
+    <TweetComponent />
+  </div>
 </template>
 
 <script setup lang="ts">
 import SimpleCounter from './features/counter/views/SimpleCounter.vue';
+import TweetComponent from './features/tweet/views/TweetComponent.vue';
 </script>
