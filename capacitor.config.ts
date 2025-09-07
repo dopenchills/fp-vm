d09c2930de9321f8d9f0a4c86476b2658eee1c52
dopenchills <dopenchills@gmail.com>
2025-09-07 11:56:09 +0900

    update appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ryo_sakaguchi.fp_vm',
+  appId: 'com.dopenchills.fpvm',
   appName: 'fp-vm',
   webDir: 'dist'
 };
